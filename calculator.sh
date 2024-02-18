07edb865d89ad588a9b81f11c488272d25861d4c
Sowmyakommireddy <sowmya.kommireddy96@gmail.com>
2024-02-18 00:37:41 -0600

    added changes in the main

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,12 @@
-add, sub, mul, divison, percentage
+# addition function
 x=a+b
+# substraction function
 y=a-b
+# multiplication function
 z=a*b
+# division function
 w=a/b
+# percentage function
 s=a%b
+
+
